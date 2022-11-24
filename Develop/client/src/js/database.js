9b776acd376c7a9e112a124cc879163d251a1175
Nathan Aramyan <nathan.aramyan@gmail.com>
2022-11-24 12:32:49 -0700

    working through webpack and ensuring correct plug ins

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -23,7 +23,13 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
 
+  return result;
 }
 
 initdb();
