b81b7249fa12a50901209bc6e39b33ff46c7ac53
Nathan Aramyan <nathan.aramyan@gmail.com>
2022-11-24 12:42:11 -0700

    updated package.json to reflect proper scripts for running program

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -35,9 +35,9 @@ module.exports = () => {
           new WebpackPwaManifest({
             fingerprints: false,
             inject: true,
-            name: 'Contact Cards',
-            short_name: 'Contact',
-            description: 'Never forget your contacts!',
+            name: 'Just Another Text Editor',
+            short_name: 'JATE',
+            description: 'Another Text Editor-PWA',
             background_color: '#225ca3',
             theme_color: '#225ca3',
             start_url: './',
